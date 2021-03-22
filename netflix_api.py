18f8e54eb1fa394fa8015960e3faf0573081d46d
Vikram Oruganti <vikramoruganti@gmail.com>
2021-03-22 15:57:03 -0400

    Update netflix_api.py

--- netflix_api.py
+++ netflix_api.py
@@ -7,7 +7,7 @@ from firebase_admin import firestore
 conn = http.client.HTTPSConnection("unogsng.p.rapidapi.com")
 
 headers = {
-    'x-rapidapi-key': "1ea5976ebdmsh8a21d79f662bac1p135ff2jsn1209a877a93b",
+    'x-rapidapi-key': "***********************************",
     'x-rapidapi-host': "unogsng.p.rapidapi.com"
     }
 
@@ -19,7 +19,7 @@ json_data = json.loads(data)
 movies_data=json_data["results"]
 
 if not firebase_admin._apps:
-    cred = credentials.Certificate(r'C:\Users\Vikram\Happy-Android-Backend\happy-2a3a7-firebase-adminsdk-4agsy-bbc34423a2.json') 
+    cred = credentials.Certificate(r'C:\Users\Vikram\Happy-Android-Backend\happy-*****-firebase-adminsdk***************.json') 
     firebase_admin.initialize_app(cred)
 
 db = firestore.client()
